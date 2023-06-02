01d366219fd1f40bb7a15b1aff31b0465df38526
steven161 <steven.nguyen161@gmail.com>
2023-06-02 14:14:26 +0700

    Update hello-yocto.c

--- hello-yocto.c
+++ hello-yocto.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("Hello, Yocto from an application cloned from Github!\r\n");
+	printf("Hello, Yocto [Version Update 2] from an application cloned from Github!\r\n");
 	return 0;
 }
